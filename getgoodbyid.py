f268bd1af261f090e0d840b572a5cdc490c4c262
laser <sfera.laser@gmail.com>
2016-11-21 21:07:31 +0200

    404 + index text

--- getgoodbyid.py
+++ getgoodbyid.py
@@ -16,6 +16,8 @@ class GetGoodById():
         mycon.connect_to_db('falcontexample.mysql.pythonanywhere-services.com', 'falcontexample',
                     'sfera1488', 'falcontexample$restfulshopdb')
         res = mycon.get_data('SELECT good_name, date, in_shop, good_price FROM goods WHERE good_id = ' + goodid)
+        if not res:
+            raise falcon.HTTPError(falcon.HTTP_404, '404', 'Cant`t found this object')
         resp.set_header('Content-Type', 'application/json; charset=utf-8')
         resp.status = falcon.HTTP_200
         resp.body = json.dumps(res, default=my_date)
--- index.py
+++ index.py
@@ -3,10 +3,14 @@ import falcon
 class IndexResource(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
-        resp.body = ('\nHello world!\n'
-                     '\n This is my firs falcon web api'
-                     '\n This is sample of simple shop model'
-                     '\n "/goods" - GET all goods'
-                     '\n "goods/{id}" - GET good by ID'
+        resp.body = ('\n Hello, world!\n'
+                     '\n This is my first falcon web api'
+                     '\n This is the sample of a simple shop model'
+                     '\n "/goods" - GET all products'
+                     '\n You may add any product in the table by the POST method from "/goods"'
+                     '\n you have to send json (for example) { "in_shop": 1/0 *product availability*, '
+                     '"good_name": "your product name", "good_price": *your product price*}'
+                     '\n "goods/{id}" - GET products by ID'
+                     '\n '
                      )
 
